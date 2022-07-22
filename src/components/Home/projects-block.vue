64649555a6d1a799d45ab39a0bfb4b0e493789cb
Vladislav Shell <shell-vladislav@mail.ru>
2022-07-22 18:02:23 +0700

    dd github api

--- src/components/Home/projects-block.vue
+++ src/components/Home/projects-block.vue
@@ -3,17 +3,23 @@
     <div class="container">
       <h2 class="projects-block__title">Projects</h2>
       <div class="projects-block__list">
-        <div class="projects-block__list-item" v-for="(project, index) in projectsList" :key="index">
-          <picture class="list-item__picture">
-            <source type="image/webp" :srcset="project.image">
-            <img :src="project.image" alt="Main photo">
-          </picture>
+        <div class="projects-block__list-item" v-for="(project, index) in type === 'home-page' ? projectsList.slice(0, 2) : projectsList" :key="index">
+<!--          <picture class="list-item__picture">-->
+<!--            <source type="image/webp" :srcset="project.image">-->
+<!--            <img :src="project.image" alt="Main photo">-->
+<!--          </picture>-->
           <div class="list-item__info">
+
             <h2 class="list-item__info-title">
-              {{project.title}}
+              <a rel="noopener noreferrer nofollow"
+                 :href="project.html_url"
+                 target="_blank"
+              >
+                {{project.name}}
+              </a>
             </h2>
             <div class="list-item__info-tags">
-              <div class="list-item__info-tags-item" v-for="(tag, index) in project.tags" :key="index">
+              <div class="list-item__info-tags-item" v-for="(tag, index) in Object.keys(project.tags)" :key="index">
                 {{tag}}
               </div>
             </div>
@@ -33,31 +39,38 @@
 </template>
 
 <script lang="ts" setup>
+import { onMounted, reactive } from "vue";
+import axios from "axios";
+
 const props = defineProps({
   type: { type: String, required: true }
 })
 
-const projectsList = [
-  {
-    title: 'TITLE PROJECT',
-    image: '',
-    tags: [
-      'JavaScript',
-      'TypeScript',
-      'Vue'
-    ],
-    description: 'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.'
-  },
-  {
-    title: 'I N S I G H T G R A M',
-    image: '',
-    tags: [
-      'TypeScript',
-      'Vue'
-    ],
-    description: 'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.'
-  }
-]
+let projectsList = reactive([])
+
+const getProjects = async () => {
+  await axios.get('https://api.github.com/users/FreeeeZ/repos')
+    .then(function (response) {
+      for (let i = 0; i < response.data.length; i++) {
+        projectsList.push(response.data[i])
+
+        axios.get(`https://api.github.com/repos/FreeeeZ/${response.data[i].name}/languages`)
+          .then(function (response) {
+            projectsList[i].tags = response.data
+          })
+          .catch(function (error) {
+            console.log(error);
+          })
+      }
+    })
+    .catch(function (error) {
+      console.log(error);
+    })
+};
+
+onMounted(() => {
+  getProjects()
+})
 </script>
 
 <style lang="scss" scoped>
