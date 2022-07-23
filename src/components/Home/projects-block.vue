4a683f18300c0dd5e4318b194a31d76689b16594
Vladislav Shell <shell-vladislav@mail.ru>
2022-07-23 14:55:59 +0700

    fix errors

--- src/components/Home/projects-block.vue
+++ src/components/Home/projects-block.vue
@@ -18,12 +18,12 @@
                 {{project.name}}
               </a>
             </h2>
-            <div class="list-item__info-tags">
+            <div class="list-item__info-tags" v-if="project.tags ? Object.keys(project.tags).length : ''">
               <div class="list-item__info-tags-item" v-for="(tag, index) in Object.keys(project.tags)" :key="index">
                 {{tag}}
               </div>
             </div>
-            <p class="list-item__info-description">
+            <p class="list-item__info-description" v-if="project.description">
               {{project.description}}
             </p>
           </div>
@@ -46,7 +46,7 @@ const props = defineProps({
   type: { type: String, required: true }
 })
 
-// TODO: Вынести в class
+// TODO: Put in class
 
 interface Owner {
   login: string;
@@ -151,7 +151,7 @@ interface ProjectsObject {
   tags: string[]
 }
 
-// TODO: Вынести в class
+// TODO: Put in class
 
 let projectsList: Array<ProjectsObject> = reactive([])
 
