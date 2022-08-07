70aed41562406d04d9e233990dd675d89ad83b92
Vladislav Shell <shell-vladislav@mail.ru>
2022-08-07 17:12:27 +0700

    change home page structure

--- src/components/not-found/not-found-wrap.vue
+++ src/components/not-found/not-found-wrap.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="notfound-wrap">
+  <div class="not-found-wrap">
     <div class="container">
-      <picture class="notfound-wrap__picture">
+      <picture class="not-found-wrap__picture">
         <source type="image/webp" srcset="@/assets/images/global/whale-404.webp">
         <img src="@/assets/images/global/whale-404.svg" alt="Whale">
       </picture>
-      <h1 class="notfound-wrap__title">404</h1>
-      <h2 class="notfound-wrap__subtitle">Oops! You were not supposed to find a crying whale.</h2>
+      <h1 class="not-found-wrap__title">404</h1>
+      <h2 class="not-found-wrap__subtitle">Oops! You were not supposed to find a crying whale.</h2>
       <router-link to="/" class="button button-primary">
         Go to main page
       </router-link>
@@ -15,5 +15,5 @@
 </template>
 
 <style lang="scss" scoped>
-@use './styles/notfound-wrap.scss';
+@use './styles/not-found-wrap.scss';
 </style>
