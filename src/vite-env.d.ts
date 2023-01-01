215408bc5b7d9b3e82250a7cdf76b3ab0b9f05fa
Vladislav Shell <shell.vladislav97@gmail.com>
2023-01-02 01:27:35 +0700

    Fix vue shims

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,9 +1,10 @@
 /// <reference types="vite/client" />
 
 declare module '*.vue' {
-  import type { DefineComponent } from 'vue'
-  const component: DefineComponent<{}, {}, any>
-  export default component
+  import type { defineComponent } from 'vue'
+
+  const component: ReturnType<typeof defineComponent>;
+  export default component;
 }
 
 declare module 'click-outside-vue3';
